10875acfd8b31c0b32fc41c553c030bfbb02ef48
AlanWangsgard <alanwangsgard@byui.edu>
2022-07-08 15:28:14 -0600

    updated build

--- build/js/main.js
+++ build/js/main.js
@@ -15,9 +15,12 @@ async function load() {
     p.classList.add("iconNumber")
     icon.prepend(p)
     cartIconValue()
+    document.querySelector(".SearchButton").addEventListener("click", function() { window.location.href = "../product-listing/index.html?category=" + document.querySelector(".selectCategory").value + "&searchTerm=" + document.querySelector(".itemSearch").value })
+
 }
 
 load()
+
 
 // const listElement = document.querySelector('.product-list');
 // const myList = new ProductList('tents', dataSource, listElement);
